8aa5a96cd4883fae4114d7b37dbab805ee86930a
Sancokai <pablo.peiro@epitech.eu>
2024-11-19 14:30:25 +0100

    feat(parallax): Define ParallaxBackground header

---
+++ client/include/ParallaxBackground.hpp
@@ -0,0 +1,28 @@
+#ifndef PARALLAX_BACKGROUND_HPP_
+    #define PARALLAX_BACKGROUND_HPP_
+
+    #include <SFML/Graphics.hpp>
+    #include <vector>
+
+class ParallaxBackground {
+    public:
+        struct Layer {
+            sf::Sprite sprite;
+            float speed;
+            float offset;
+        };
+
+        ParallaxBackground(const sf::Vector2u& windowSize);
+        void update(float deltaTime);
+        void render(sf::RenderWindow& window);
+
+    private:
+        std::vector<Layer> m_layers;
+        std::vector<sf::Texture> m_textures;
+        sf::Vector2u m_windowSize;
+
+        void setupLayer(const std::string& texturePath, float speed);
+        void updateLayerScale(sf::Sprite& sprite);
+};
+
+#endif // PARALLAX_BACKGROUND_HPP
