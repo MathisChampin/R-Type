7b695458414cb789863da78d053b2d1ee07c3964
Mathis Champin <mathis.champin@epitech.eu>
2024-12-10 17:40:59 +0100

    feat(ECS): add units tests for create player and ennemies

--- ECS/src/system/ennemies_system.cpp
+++ ECS/src/system/ennemies_system.cpp
@@ -20,11 +20,9 @@ void System::ennemies_system(registry &reg)
     int y = dist(rng);
 
     reg.add_component<component::attribute>(ennemies, {component::attribute::Ennemies});
-    reg.add_component<component::controllable>(ennemies, {component::controllable::NoKey});
     reg.add_component<component::level>(ennemies, {component::level::Level0});
     reg.add_component<component::life>(ennemies, {1});
     reg.add_component<component::position>(ennemies, {1920, y});
-    reg.add_component<component::score>(ennemies, {0});
     reg.add_component<component::size>(ennemies, {32, 14}); // remplacer par size de la sprite de l'ennemie
     reg.add_component<component::state>(ennemies, {component::state::Alive});
     reg.add_component<component::velocity>(ennemies, {20, 0});
--- ECS/tests/system/testsCreateEntity.cpp
+++ ECS/tests/system/testsCreateEntity.cpp
@@ -9,6 +9,7 @@
 #include "size.hpp"
 #include "state.hpp"
 #include "velocity.hpp"
+#include "Entity.hpp"
 
 #include <criterion/criterion.h>
 
@@ -17,8 +18,8 @@ Test(PlayerSystem, tests_registry_player) {
     System sys;
 
     sys.registry_system(reg);
-    sys.ennemies_system(reg);
     sys.player_system(reg);
+    sys.ennemies_system(reg);
 
     auto &attributes = reg.get_components<component::attribute>();
     auto &positions = reg.get_components<component::position>();
@@ -29,31 +30,35 @@ Test(PlayerSystem, tests_registry_player) {
     auto &score = reg.get_components<component::score>();
     auto &size = reg.get_components<component::size>();
     auto &state = reg.get_components<component::state>();
+    Entity player;
 
     for (size_t i = 0; i < attributes.size(); i++) {
         auto &a = attributes[i];
         if (a._type == component::attribute::Player) {
-            auto &pos = positions[i];
-            auto &vel = velocity[i];
-            auto &ctrl = controllable[i];
-            auto &l = level[i];
-            auto &lf = life[i];
-            auto &s = score[i];
-            auto &sz = size[i];
-            auto &st = state[i];
-            cr_assert_eq(vel.x, 0, "Expected velocity x to be 0, but got %d", vel.x);
-            cr_assert_eq(vel.y, 0, "Expected velocity y to be 0, but got %d", vel.y);
-            cr_assert_eq(pos.x, 50, "Expected position x to be 50, but got %d", pos.x);
-            cr_assert_eq(pos.y, 500, "Expected position y to be 500, but got %d", pos.y);
-            cr_assert_eq(ctrl.active_key, 4, "Expected controllable to be 4, but got %d", ctrl.active_key);
-            cr_assert_eq(l._levelKey, 0, "Expected level to be 0, but got %d", l._levelKey);
-            cr_assert_eq(lf.life, 4, "Expected life to be 4, but got %d", lf.life);
-            cr_assert_eq(s.score, 0, "Expected score to be 0, but got %d", s.score);
-            cr_assert_eq(sz.x, 32, "Expected life to be 32, but got %d", sz.x);
-            cr_assert_eq(sz.y, 14, "Expected life to be 14, but got %d", sz.y);
-            cr_assert_eq(st._stateKey, 1, "Expected state to be 1, but got %d", st._stateKey);
+            player = reg.get_entity(i);
         }
     }
+
+    auto &pos = positions[player.get_id()];
+    auto &vel = velocity[player.get_id()];
+    auto &ctrl = controllable[player.get_id()];
+    auto &l = level[player.get_id()];
+    auto &lf = life[player.get_id()];
+    auto &s = score[player.get_id()];
+    auto &sz = size[player.get_id()];
+    auto &st = state[player.get_id()];
+
+    cr_assert_eq(vel.x, 0, "Expected velocity x to be 0, but got %d", vel.x);
+    cr_assert_eq(vel.y, 0, "Expected velocity y to be 0, but got %d", vel.y);
+    cr_assert_eq(pos.x, 50, "Expected position x to be 50, but got %d", pos.x);
+    cr_assert_eq(pos.y, 500, "Expected position y to be 500, but got %d", pos.y);
+    cr_assert_eq(ctrl.active_key, 4, "Expected controllable to be 4, but got %d", ctrl.active_key);
+    cr_assert_eq(l._levelKey, 0, "Expected level to be 0, but got %d", l._levelKey);
+    cr_assert_eq(lf.life, 4, "Expected life to be 4, but got %d", lf.life);
+    cr_assert_eq(s.score, 0, "Expected score to be 0, but got %d", s.score);
+    cr_assert_eq(sz.x, 32, "Expected life to be 32, but got %d", sz.x);
+    cr_assert_eq(sz.y, 14, "Expected life to be 14, but got %d", sz.y);
+    cr_assert_eq(st._stateKey, 1, "Expected state to be 1, but got %d", st._stateKey);
 }
 
 Test(PlayerSystem, tests_registry_ennemies) {
@@ -67,34 +72,32 @@ Test(PlayerSystem, tests_registry_ennemies) {
     auto &attributes = reg.get_components<component::attribute>();
     auto &positions = reg.get_components<component::position>();
     auto &velocity = reg.get_components<component::velocity>();
-    auto &controllable = reg.get_components<component::controllable>();
     auto &level = reg.get_components<component::level>();
     auto &life = reg.get_components<component::life>();
-    auto &score = reg.get_components<component::score>();
     auto &size = reg.get_components<component::size>();
     auto &state = reg.get_components<component::state>();
+    Entity ennemies;
 
     for (size_t i = 0; i < attributes.size(); i++) {
         auto &a = attributes[i];
         if (a._type == component::attribute::Ennemies) {
-            auto &pos = positions[i];
-            auto &vel = velocity[i];
-            auto &ctrl = controllable[i];
-            auto &l = level[i];
-            auto &lf = life[i];
-            auto &s = score[i];
-            auto &sz = size[i];
-            auto &st = state[i];
-            cr_assert_eq(vel.x, 20, "Expected velocity x to be 20, but got %d", vel.x);
-            cr_assert_eq(vel.y, 0, "Expected velocity y to be 0, but got %d", vel.y);
-            cr_assert_eq(pos.x, 1920, "Expected position x to be 1920, but got %d", pos.x);
-            cr_assert_eq(ctrl.active_key, 4, "Expected controllable to be 4, but got %d", ctrl.active_key);
-            cr_assert_eq(l._levelKey, 0, "Expected level to be 0, but got %d", l._levelKey);
-            cr_assert_eq(lf.life, 1, "Expected life to be 1, but got %d", lf.life);
-            cr_assert_eq(s.score, 0, "Expected score to be 0, but got %d", s.score);
-            cr_assert_eq(sz.x, 32, "Expected life to be 32, but got %d", sz.x);
-            cr_assert_eq(sz.y, 14, "Expected life to be 14, but got %d", sz.y);
-            cr_assert_eq(st._stateKey, 1, "Expected state to be 1, but got %d", st._stateKey);
+            ennemies = reg.get_entity(i);
         }
     }
+
+    auto &pos = positions[ennemies.get_id()];
+    auto &vel = velocity[ennemies.get_id()];
+    auto &l = level[ennemies.get_id()];
+    auto &lf = life[ennemies.get_id()];
+    auto &sz = size[ennemies.get_id()];
+    auto &st = state[ennemies.get_id()];
+
+    cr_assert_eq(vel.x, 20, "Expected velocity x to be 20, but got %d", vel.x);
+    cr_assert_eq(vel.y, 0, "Expected velocity y to be 0, but got %d", vel.y);
+    cr_assert_eq(pos.x, 1920, "Expected position x to be 1920, but got %d", pos.x);
+    cr_assert_eq(l._levelKey, 0, "Expected level to be 0, but got %d", l._levelKey);
+    cr_assert_eq(lf.life, 1, "Expected life to be 1, but got %d", lf.life);
+    cr_assert_eq(sz.x, 32, "Expected life to be 32, but got %d", sz.x);
+    cr_assert_eq(sz.y, 14, "Expected life to be 14, but got %d", sz.y);
+    cr_assert_eq(st._stateKey, 1, "Expected state to be 1, but got %d", st._stateKey);
 }
