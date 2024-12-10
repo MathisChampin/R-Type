f1634f4827ddd264626ce9277898a041ad4eef69
Mathis Champin <mathis.champin@epitech.eu>
2024-12-10 18:05:52 +0100

    feat(ECS): delete function for create player and ennemies

--- ECS/include/system/system.hpp
+++ ECS/include/system/system.hpp
@@ -9,9 +9,6 @@ class System {
         void control_system(registry &reg);
         void collision_system(registry &reg);
         void level_system(registry &reg);
-        void registry_system(registry &reg);
-        void player_system(registry &reg);
-        void ennemies_system(registry &reg);
 };
 
 #endif /* SYSTEM_HPP_ */
