eb7f96d690a67c99c7fa5af09b84e92dfd7cc0ab
AlexandreLorgeoux <alexandre.lorgeoux@epitech.eu>
2025-01-13 16:18:47 +0100

    fix(client): init score when nothing sent by server

--- client/src/Score.cpp
+++ client/src/Score.cpp
@@ -11,8 +11,15 @@ void Score::initialize(const sf::Font &font, unsigned int characterSize, const s
     m_scoreText.setCharacterSize(characterSize);
     m_scoreText.setPosition(position);
     m_scoreText.setFillColor(sf::Color::White);
-    updateScore(0);
+
+    m_currentScore = 0;
+    m_scoreText.setString("Score: 0");
+
+    sf::FloatRect textRect = m_scoreText.getLocalBounds();
+    m_scoreText.setOrigin(textRect.width, 0);
+    m_scoreText.setPosition(sf::Vector2f(1920 - 10, position.y));
 }
+
 
 void Score::updateScore(int newScore) {
     if (newScore < 0) {
